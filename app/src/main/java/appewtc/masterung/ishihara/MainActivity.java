5e0af118b77f55f6a3745abbaa5d363563e5190d
masterUNG <phrombutr@gmail.com>
2015-05-25 16:03:02 +0700

    Day1

--- app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
@@ -1,17 +1,125 @@
 package appewtc.masterung.ishihara;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private TextView questionTextView;
+    private ImageView ishiharaImageView;
+    private RadioGroup choiceRadioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton;
+    private Button answerButton;
+    private int radioAnInt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+        //Radio Controller
+        radioController();
+
+    }   //onCreate
+
+    private void radioController() {
+
+        choiceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+
+                soundEffect();
+
+                //Setup radioAnInt
+                switch (checkedID) {
+                    case R.id.radioButton:
+                        radioAnInt = 1;
+                        break;
+                    case R.id.radioButton2:
+                        radioAnInt = 2;
+                        break;
+                    case R.id.radioButton3:
+                        radioAnInt = 3;
+                        break;
+                    case R.id.radioButton4:
+                        radioAnInt = 4;
+                        break;
+                    default:
+                        radioAnInt = 0;
+                        break;
+                }
+
+            }   // event
+        });
+
+    }   // radioController
+
+    private void buttonController() {
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                soundEffect();
+
+                //Check Zero
+                checkZero();
+
+            }   // event
+        });
+
+    }   // buttonController
+
+    private void checkZero() {
+
+        if (radioAnInt == 0) {
+
+            Toast.makeText(MainActivity.this, "กรุณาตอบคำถาม ด้วยคะ", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+        }
+
+    }   //checkZero
+
+    private void soundEffect() {
+
+        MediaPlayer myMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+        myMediaPlayer.start();
+    }
+
+    private void bindWidget() {
+
+        questionTextView = (TextView) findViewById(R.id.textView2);
+        ishiharaImageView = (ImageView) findViewById(R.id.imageView);
+        choiceRadioGroup = (RadioGroup) findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        answerButton = (Button) findViewById(R.id.button);
+
     }
 
     @Override
@@ -35,4 +143,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   //Main Class
